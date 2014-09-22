c7e4ca66e87e699cd7072e9c521fa733153998c4
jonschlinkert <dev@sellside.com>
2014-09-22 03:14:18 -0400

    docs, run verb

--- test.js
+++ test.js
@@ -8,20 +8,31 @@
 'use strict';
 
 var should = require('should');
-var pick = require('./');
+var get = require('./');
 
-describe('.pick()', function () {
-  it('should return an object with keys specified as strings.', function () {
-    pick({a: 'a', b: 'b'}, 'a').should.eql({a: 'a'});
+describe('.get()', function () {
+  it('should get the specified property.', function () {
+    get({a: 'a', b: 'b'}, 'a').should.eql({a: 'a'});
   });
 
-  it('should return an object with keys specified as arrays.', function () {
-    pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b']).should.eql({a: 'a', b: 'b'});
-    pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar']).should.eql({foo: 'foo', bar: 'bar'});
+  it('should get a value only.', function () {
+    get({a: 'a', b: {c: 'd'}}, 'a', true).should.eql('a');
   });
 
-  it('should ignore keys that don not exist.', function () {
-    pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b', 'foo']).should.eql({a: 'a', b: 'b'});
-    pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar', 'abc']).should.eql({foo: 'foo', bar: 'bar'});
+  it('should get a nested property.', function () {
+    get({a: 'a', b: {c: 'd'}}, 'b.c').should.eql({c: 'd'});
+  });
+
+  it('should get a value only.', function () {
+    get({a: 'a', b: {c: 'd'}}, 'b.c', true).should.eql('d');
+  });
+
+  it('should get a deeply nested property.', function () {
+    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql({h: 'i'});
+  });
+
+  it('should get the value of a deeply nested property.', function () {
+    get({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h', true).should.eql('i');
   });
 });
+
